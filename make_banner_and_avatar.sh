ed02800f239480e8a8e5fb568dfb6e647d602c95
gar <hrothgarrrr@gmail.com>
2026-01-27 10:50:24 -0600

    a few updates: added banner-logo, banner-logomark; renamed some things; updated grassy banner

--- make_banner_and_avatar.sh
+++ make_banner_and_avatar.sh
@@ -5,21 +5,21 @@ cd "$(dirname "$0")"
 
 # Twitter banner: 1500x500
 # Scale banner.png 15x, crop centered
-magick banner.png -filter point -resize 1500% -gravity center -crop 1500x500+0+0 +repage twitter_banner.png
-echo "Created twitter_banner.png (1500x500)"
+magick banner-grass-1x.png -filter point -resize 1500% \
+  -gravity center -crop 1500x500+0+0 +repage banner-grass.png
+echo "Created banner-grass.png (1500x500)"
 
 # Twitter avatar: 400x400
 # Calculate minimum scale factor so both dimensions >= 400px
-read width height <<< $(magick identify -format "%w %h" avatar.png)
+read width height <<< $(magick identify -format "%w %h" avatar-1x.png)
 scale_w=$(( (400 + width - 1) / width ))  # ceil(400/width)
 scale_h=$(( (400 + height - 1) / height ))  # ceil(400/height)
 scale=$(( scale_w > scale_h ? scale_w : scale_h ))  # max
 percent=$(( scale * 100 ))
 
-magick avatar.png -filter point -resize ${percent}% -gravity center -crop 400x400+0+0 +repage twitter_avatar.png
-magick avatar.png -filter point -resize ${percent}% \
+magick avatar-1x.png -filter point -resize ${percent}% \
   -gravity center -crop 400x420+0+0 +repage \
   -gravity north -crop 400x400+0+0 +repage \
-  twitter_avatar.png
+  avatar.png
 
-echo "Created twitter_avatar.png (400x400) - scaled ${scale}x"
+echo "Created avatar.png (400x400) - scaled ${scale}x"
